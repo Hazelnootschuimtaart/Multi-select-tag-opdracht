4054198d2981927948f96f1b287f9b7ae91785af
Louise Schendelaar <louise.schendelaar@stichtingheldeninit.nl>
2023-11-27 14:31:16 +0100

    Verwijderen van laatste tag met backspace

--- src/Home.js
+++ src/Home.js
@@ -8,56 +8,73 @@ import data from "./dbTags.json";
 export default function Home() {
     const allTags = data.tags;
     const [inputValue, setInputValue] = useState(""); // Typed value in searchbar
-    const [tagData, setTagData] = useState(allTags); // Collection of tags that match the search value (all tags if no search value)
-    const [clickedTag, setClickedTag] = useState();
-    const [clickedTags, setClickedTags] = useState([]);
+    const [tagData, setTagData] = useState(allTags); // Collection of tags that match the search value (all tags if no search value). 
+    const [clickedTag, setClickedTag] = useState(); // The current clicked tag
+    const [clickedTags, setClickedTags] = useState([]); // All tags that have to be shown as 'selected/clicked' in the searchbar
+    // const [removeLastItem, setRemoveLastItem] = useState(false);
     const maxNumberTags = 4; // True number is +1 higher, so entering 4 is actually max 5 items
 
+    // checks if the last clicked tag already exists in clickedTags array. If true, clickedTag is set to undefined. If false, the clickedTags-array is updated with the new clicked tag.
     useEffect(() => {
-        // node.js aanklikken, nog een keer aanklikken, verwijderen, en dan weer aanklikken. Dan doet ie het niet. 
-        // Na saven komt hij regelmatig ook weer terug dus dan zit hij nog in de clickedtagsarray
         let checkTags = clickedTags.find((tag) => {
             return tag === clickedTag;
         })
 
-        console.log("clickedTag ", clickedTag);
-        console.log("checkTags ", checkTags);
-
         if (checkTags == clickedTag) {
             setClickedTag(undefined);
         }
-        // deze comments mogen eruit als alles de volgende keer nog werkt
+
         if (clickedTag !== undefined && clickedTags.length <= maxNumberTags && checkTags === undefined) {
-            setClickedTags((prevState) => ([...prevState, clickedTag])); // zorgt ervoor dat de clickedTag alsnog wordt toegevoegd!!! Ook als ie verwijderd is!
-            setClickedTag(undefined); // deze komt te laat!
+            setClickedTags((prevState) => ([...prevState, clickedTag]));
+            setClickedTag(undefined);
         }
     }, [clickedTag]);
-    console.log("clickedTags ", clickedTags);
 
-    const inputChange = (inputText) => { // this is called if inputfield changes
+    // this is called if inputfield changes
+    const inputChange = (inputText) => {
         setInputValue(inputText);
-        setTagData(allTags); // tagData is filled with all tags, so the filter will always work with/filter al the tags.
+        setTagData(allTags); // tagData is filled with all tags, so the filter will always work with/filter all the tags.
     }
+
+    // const removeLastTag = (backspacePressed) => {
+    //     if (inputValue == "" && backspacePressed == true) {
+    //         const oldArray = clickedTags;
+    //         oldArray.pop();
+    //         console.log(oldArray);
+    //         setClickedTags(oldArray);
+    //         setClickedTag(undefined);
+    //     }
+    // }
+
+
 
     // filters the tags that include the inputvalue and updates the tagData state to only show the ones that match with the inputvalue
     useEffect(() => {
         if (inputValue !== "") {
             const inputValueLow = inputValue.toLowerCase();
+            // This filter will always filter ALL the tags.
             const filteredTags = (tagData || []).filter((tag) => {
                 const tagTitleLow = tag.title.toLowerCase();
-                const includesName = tagTitleLow.includes(inputValueLow); //checks if inputvalue == itemname (returns true/false)
+                const includesName = tagTitleLow.includes(inputValueLow); //checks if inputvalue == tagname (returns true/false)
                 return includesName === true; //returns array with matched items
             });
             setTagData(filteredTags); //Updates state with array that contains matched items (or all if inputvalue is empty)
         }
     }, [inputValue]);
 
+    console.log("clickedTags ", clickedTags);
+
+    // moet opnieuw renderen, want verandering is nog niet zichtbaar (wel te zien in log).
+    // maar eigenlijk doe ik precies hetzelfde als met het wegklikken met kruisje, waarom doet hij het dan nu niet met de backspace-methode?
+
+
+
     return (
         <>
             <h4 className={styles.header}>Tags</h4>
             <p className={styles.description}>Add up to 5 tags. Start typing to see suggestions.</p>
             <div className={styles.searchContainer} style={{ width: "55%" }}>
-                <SearchBar value={inputValue} handleChange={(value) => { inputChange(value) }} clickedTags={clickedTags} arrayWithoutRemovedItem={(array) => { setClickedTags(array) }} />
+                <SearchBar value={inputValue} removeLastTag={(newTagsArray) => { setClickedTags(newTagsArray) }} handleChange={(value, backspace) => { inputChange(value, backspace) }} clickedTags={clickedTags} arrayWithoutRemovedItem={(array) => { setClickedTags(array) }} />
                 {inputValue.length !== 0 &&
                     <ShowTags inputText={inputValue} data={tagData} clickedElement={(title) => { setClickedTag(title) }} />
                 }
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,8 +1,31 @@
+import { useState } from 'react';
 import styles from './searchBar.module.css';
 
 export default function SearchBar(props) {
     let inputValue = props.value;
     let clickedTags = props.clickedTags;
+    const [arrayWithRemovedTag, setArrayWithRemovedTag] = useState([]);
+
+    let checkKey = (e) => {
+        if (e.key === "Backspace") {
+            removeLastItem(true);
+        }
+        else {
+            removeLastItem(false);
+        }
+    }
+
+    const removeLastItem = (backspacePressed) => {
+        if (inputValue == "" && backspacePressed == true) {
+            const oldArray = clickedTags;
+            oldArray.pop();
+            console.log(oldArray);
+            props.removeLastTag(oldArray);
+            setArrayWithRemovedTag(oldArray); // nu haalt ie alleen de eerste weg...
+        }
+    }
+
+    console.log(arrayWithRemovedTag);
 
     const handleChange = (e) => {
         props.handleChange(e.target.value);
@@ -11,12 +34,8 @@ export default function SearchBar(props) {
     const removeClick = (tag) => {
         console.log(tag);
         const filteredArray = clickedTags.filter((tagItem) => {
-            console.log(tagItem);
-            // const removeTag = tagItem.includes(tag); // this returns true if the x-clicked tag matches with the tag in the array, so then it should be removed. If false, it should be kept.
-            // console.log("removeTag ", removeTag);
             return tagItem !== tag;
         })
-        console.log("array zonder removed item ", filteredArray);
         props.arrayWithoutRemovedItem(filteredArray);
     }
 
@@ -31,7 +50,7 @@ export default function SearchBar(props) {
                         </div>
                     )
                 })}
-                <input type='text' className={styles.inputElement} value={inputValue} onChange={(e) => { handleChange(e) }}></input>
+                <input type='text' className={styles.inputElement} value={inputValue} onKeyDown={(e) => checkKey(e)} onChange={(e) => { handleChange(e) }}></input>
             </div>
         </>
     )
